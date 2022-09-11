3e6b589aa943b5441f0a251fcc3452ebbf2602e0
abbasmurudkar <abbasmurudkar18@icloud.com>
2022-09-11 18:52:38 +0530

    Initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,33 @@
 
 const password = document.getElementById('Password')
 const Confirm = document.getElementById('C-Password')
-
 function showHide(){
 if(password.type === 'password'){
 password.setAttribute("type","text")
+document.getElementById('icons').className="fa-solid fa-unlock"
 }
 else{
 password.setAttribute("type","password")
+document.getElementById('icons').className="fa-solid fa-lock"
+
 }
 }
 function ConfirmPassword(){
     if(Confirm.type==="password"){
         Confirm.setAttribute("type","text")
+        document.getElementById('icon').className="fa-solid fa-unlock"
     }
     else{
         Confirm.setAttribute("type","password")
+        document.getElementById('icon').className="fa-solid fa-lock"
         }
 }
+function login(){
+    alert("Successfully login");
+}
+function Register(){
+    alert("Successfully Register")
+}
+
 
 
